b787fd5af3f52cb35dcd113c13149011c4e524fe
chick307 <chick307@gmail.com>
2021-08-05 07:42:58 +0900

    Remove power-assert

--- test/symbols-test.js
+++ test/symbols-test.js
@@ -1,7 +1,7 @@
 // (C) 2015 chick307 <chick307@gmail.com>
 // Licensed under the MIT License. http://chick307.mit-license.org/
 
-import assert from 'power-assert';
+import assert from 'assert';
 
 import defaultExports, * as exports from '../src/di';
 
